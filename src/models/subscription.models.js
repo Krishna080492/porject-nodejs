25a23c082083dcd3385f55ceae9dcaf4aab8455e
Krishna080492 <krishna.desai4@gmail.com>
2025-04-18 12:17:44 +0530

    add subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const subsctriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subsctription = mongoose.model(
+  "Subscription",
+  subsctriptionSchema
+);
